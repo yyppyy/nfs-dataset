ab844e6942043cff2c7cb1c5c32f00e560378531
Yanpeng Yu <34593175+yyppyy@users.noreply.github.com>
2021-08-18 11:20:22 +0800

    Update rename_later.sh

--- rename_later.sh
+++ rename_later.sh
@@ -3,6 +3,6 @@ sudo apt install qemu-kvm libvirt-bin bridge-utils virtinst
 #enter a Y later...
 
 #create default network
-REPO_DIR=/local/repository/
-sudo virsh net-define --file ${REPO_DIR}default.xml
-sudo virsh net-start default
+#REPO_DIR=/local/repository/
+#sudo virsh net-define --file ${REPO_DIR}default.xml
+#sudo virsh net-start default
