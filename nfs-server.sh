5e1b9151d42e4f247719718c274dca96f283822e
Yanpeng Yu <34593175+yyppyy@users.noreply.github.com>
2021-08-19 00:35:37 +0800

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -2,7 +2,7 @@ default_net=192.168.1.0/24
 trace_dir=/mydata
 
 
-sudo apt install 
+sudo apt install nfs-kernel-server
 sudo mkdir -p -m 777 ${trace_dir}
 echo "${trace_dir}  ${default_net}(rw,sync,no_subtree_check)" | sudo tee /etc/exports
 sudo exportfs -a
