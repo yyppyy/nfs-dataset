0a95a256de7ddc7d8863f7b875abc1692e1c78f4
Yanpeng Yu <34593175+yyppyy@users.noreply.github.com>
2021-08-18 11:36:51 +0800

    Update rename_later.sh

--- rename_later.sh
+++ rename_later.sh
@@ -6,3 +6,9 @@ sudo apt install qemu-kvm libvirt-bin bridge-utils virtinst
 #REPO_DIR=/local/repository/
 #sudo virsh net-define --file ${REPO_DIR}default.xml
 #sudo virsh net-start default
+
+#create VM
+num_CNs=1
+for i in $(seq 1 $num_CNs); do
+    sudo virt-install --name ubuntu_CN$i --memory 8192 --vcpus 10 --disk /nfs/vm_images/ubuntu_CN_$i.qcow --network default
+done
