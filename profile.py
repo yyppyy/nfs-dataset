c4efd59f77885371772610dc3ad48f3f3a6860e0
Yanpeng Yu <34593175+yyppyy@users.noreply.github.com>
2021-08-19 02:25:04 +0800

    Update profile.py

--- profile.py
+++ profile.py
@@ -81,6 +81,7 @@ nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repos
 # Special node that represents the ISCSI device where the dataset resides
 dsnode = request.RemoteBlockstore("dsnode", nfsDirectory)
 dsnode.dataset = params.dataset
+'''
 
 # Link between the nfsServer and the ISCSI device that holds the dataset
 dslink = request.Link("dslink")
@@ -90,7 +91,6 @@ dslink.addInterface(nfsServer.addInterface())
 dslink.best_effort = True
 dslink.vlan_tagging = True
 dslink.link_multiplexing = True
-'''
 ################################################################## NFS for remote dataset #####################
 
 
