f51b33ef610b10f698fdcc369e130660a1a7d8d4
Yanpeng Yu <34593175+yyppyy@users.noreply.github.com>
2021-08-18 16:18:33 +0800

    Update rename_later.sh

--- rename_later.sh
+++ rename_later.sh
@@ -9,15 +9,18 @@ default_net=192.168.122.0/24
 #change sudo
 #echo 'Yanpeng   ALL=(ALL) NOPASSWD:/usr/bin/virsh, /sbin/ip' | sudo EDITOR='tee -a' visudo
 
-#install virsh and xdotool
-sudo apt install qemu-kvm libvirt-bin bridge-utils virtinst
+#install virsh and nfs
+sudo apt install qemu-kvm libvirt-bin bridge-utils virtinst nfs-kernel-server
 #enter a Y later... see the example below
 #echo "Y Y N N Y N Y Y N" | ./your_script
+
 
 #create default network
 #REPO_DIR=/local/repository/
 #sudo virsh net-define --file ${REPO_DIR}default.xml
 #sudo virsh net-start default
+
+
 
 #localize vm images
 sudo mkdir -p ${vm_dir}
@@ -25,17 +28,19 @@ for i in $(seq 1 ${num_CNs}); do
     sudo cp ${nfs_dir}vm_images/ubuntu_CN_${i}.qcow ${vm_dir}
 done
 
-#create local nfs to feed data to vms
-sudo apt install nfs-kernel-server
-sudo mkdir -p -m 777 ${trace_dir}
-sudo echo "${trace_dir}  ${default_net}(r,sync,no_subtree_check)" >> /etx/exports
-sudo exportfs -a
-sudo systemctl restart nfs-kernel-server
-#sudo ufw allow from ${default_net} to any port nfs
-#sudo ufw enable
 
 
 #create VM
 for i in $(seq 1 $num_CNs); do
     sudo virt-install --name ubuntu_CN${i} --memory ${vm_mem} --vcpus ${vm_vcpus} --disk ${vm_dir}ubuntu_CN_${i}.qcow --import --network default --os-variant ubuntu18.04 --noautoconsole
 done
+
+
+
+#create local nfs to feed data to vms
+sudo mkdir -p -m 777 ${trace_dir}
+sudo echo "${trace_dir}  ${default_net}(r,sync,no_subtree_check)" >> /etc/exports
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+#sudo ufw allow from ${default_net} to any port nfs
+#sudo ufw enable
